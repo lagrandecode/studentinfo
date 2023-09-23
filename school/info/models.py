da4320e876c2e4109368b7b6c599bab6ec4fb37b
lagrandecode <seunogunmolufirst1@gmail.com>
2023-09-23 21:19:24 +0100

    "commit"

--- school/info/models.py
+++ school/info/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_lenth=100)
+    last_name = models.CharField(max_length=100)
+
