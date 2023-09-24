bb70e0484ff9342c38db37a615af2acb49278164
lagrandecode <seunogunmolufirst1@gmail.com>
2023-09-24 21:03:58 +0100

    "commit"

--- school/info/models.py
+++ school/info/models.py
@@ -7,4 +7,7 @@ class Student(models.Model):
     first_name = models.CharField(max_length=100)
     middle_name = models.CharField(max_lenth=100)
     last_name = models.CharField(max_length=100)
+    school_class = models.CharField(max_length=20)
+    gender = models.CharField(max_length=10)
+    age = models.PositiveIntegerField()
 
