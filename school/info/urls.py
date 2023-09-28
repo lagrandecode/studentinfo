5ae89696b102ab95f5c16cbf7ba0eb3736f3e516
lagrandecode <seunogunmolufirst1@gmail.com>
2023-09-28 23:26:43 +0100

    "commit"

--- school/info/urls.py
+++ school/info/urls.py
@@ -7,4 +7,5 @@ from . import views
 
 urlpatterns = [
     path('',views.home),
+    path('index/',views.index)
 ]
--- school/info/views.py
+++ school/info/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 
 def home(request):
+
     return render(request,'home.html')
+
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
