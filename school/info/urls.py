a652293ec8a616d6a32d446059af2f58e555b009
lagrandecode <seunogunmolufirst1@gmail.com>
2023-09-29 23:10:36 +0100

    "commit"

--- school/info/urls.py
+++ school/info/urls.py
@@ -1,11 +1,14 @@
 
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 #  
 from . import views
 
 
+
 urlpatterns = [
     path('',views.home,name='home'),
     path('index/',views.index)
-]
+] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
--- school/school/settings.py
+++ school/school/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'info',
+    #third party applications 
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/var/www/static/",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
