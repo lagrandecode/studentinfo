633aa06e622f20ea544cbc05222a674fbf94340c
lagrandecode <seunogunmolufirst1@gmail.com>
2023-09-30 22:06:37 +0100

    "commit"

--- school/school/settings.py
+++ school/school/settings.py
@@ -77,10 +77,23 @@ WSGI_APPLICATION = 'school.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres', 
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost', 
+        'PORT': '5432',
     }
 }
 
