c6c4dd3da5352b9d07f2670bf2a897ba36f4c83b
lagrandecode <seunogunmolufirst1@gmail.com>
2023-10-01 15:20:09 +0100

    "commit"

--- school/info/forms.py
+++ school/info/forms.py
@@ -1,5 +1,6 @@
 from .models import Student
 from django import forms
+from django.core import validators
 
 
 
@@ -10,3 +11,11 @@ class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
         fields = '__all__'
+
+        widgets = {
+            'first_name' : forms.TextInput(attrs={'class':'form-control'}),
+            'last_name' : forms.TextInput(attrs={'class':'form-control'}),
+            'school_class' : forms.TextInput(attrs={'class':'form-control'}),
+            'gender' : forms.TextInput(attrs={'class':'form-control'}),
+            'age' : forms.TextInput(attrs={'class':'form-control'}),
+        }
--- school/info/views.py
+++ school/info/views.py
@@ -1,14 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import StudentForm
+from .models import Student
+import requests
+import io
+import uuid
+from datetime import datetime
+
 
 # Create your views here.
 
 
 
-
-
-
+BASE_URL = 'https://kc.kobotoolbox.org'
+SUMISSION_URL = f'{BASE_URL}/api/v1/submissions'
+TOKEN = '6fb29d8015dc136cba3558590282ddab7f2b24a5'
 
 
 def addStudent(request):
-    return render(request,'addstudent.html')
+    context = {}  # Define context outside the if-else block
+
+    if request.method == 'POST':
+        studentform = StudentForm(request.POST)
+        if studentform.is_valid():
+            name = studentform.cleaned_data['first_name']
+            last = studentform.cleaned_data['last_name']
+            school_class = studentform.cleaned_data['school_class']
+            gender = studentform.cleaned_data['gender']
+            age = studentform.cleaned_data['age']
+            reg = Student(first_name=name, last_name=last, school_class=school_class, gender=gender, age=age)
+            reg.save()
+
+            studentform = StudentForm()
+    else:
+        studentform = StudentForm()
+
+    context['form'] = studentform  # Update context with the form
+
+    return render(request, 'addstudent.html', context)
